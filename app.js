8f4afe4cb487be24d5a0b48381a26753f99cea2c
dagurvishal <dagurvishal2049@gmail.com>
2026-01-24 23:56:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 const SUPABASE_URL = "https://ehnkxlccztcjqznuwtto.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVobmt4bGNjenRjanF6bnV3dHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwNzEyMjcsImV4cCI6MjA4NDY0NzIyN30.EnG1ThOcPNj3mdzrTY-fwDwy5nsEW1GdOqLYgnIbthc"; // keep it here (public)
 
+
+
 const POSTERS_BUCKET = "posters";
 const REQUESTS_BUCKET = "requests";
 
